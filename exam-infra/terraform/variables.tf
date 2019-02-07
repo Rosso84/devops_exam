aa5b5ad10f348a8cdb01e0979ec542c7e5047628
Rosso84 <Roosbeh@hotmail.com>
2019-02-07 11:15:39 +0100

    first push

---
+++ exam-infra/terraform/variables.tf
@@ -0,0 +1,9 @@
+# Modify this file to make it your own. app prefix must be in lowercase
+
+variable "app_prefix" {
+  default = "movie-rest"
+}
+
+variable "pipeline_name" {
+  default = "exam-pipeline"
+}
